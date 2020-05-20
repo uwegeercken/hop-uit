a8741deb23ab9d1d4a9f04be7bdfbba0d30b0219
uwegeercken <uwegeercken@web.de>
2020-05-20 08:26:50 +0200

    wrong package

--- src/main/additional/hop-uit.sh
+++ src/main/additional/hop-uit.sh
@@ -3,7 +3,7 @@
 # Script to run the Hop Ultimate Import Tool (hop-uit)
 # The tool converts Pentaho PDI .ktr files into .hpl files for the project-hop platform.
 #
-# uwe.geercken@web.de - last update: 2020-05-18
+# uwe.geercken@web.de - last update: 2020-05-20
 
 lib_hop_uit=hop-uit-0.0.1-SNAPSHOT.jar
 
@@ -11,4 +11,4 @@ inputfolder=${1}
 outputfolder=${2}
 configfolder=${3}
 
-java -cp .:lib/*:${lib_hop_uit} org.apache.tools.ImportTool -i="${inputfolder}" -o="${outputfolder}" -c="${configfolder}"
+java -cp .:lib/*:${lib_hop_uit} com.datamelt.hop.uit.ImportTool -i="${inputfolder}" -o="${outputfolder}" -c="${configfolder}"
